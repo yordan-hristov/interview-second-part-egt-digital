4d0a3839bde7067f179bef7bfaebc76a386e480d
Yordan Hristov <y.hristov11@gmail.com>
2023-04-14 18:55:35 +0300

    feat: user type

---
+++ src/types/user/index.ts
@@ -0,0 +1,3 @@
+import type User from "./user";
+
+export default User;
---
+++ src/types/user/user.ts
@@ -0,0 +1,29 @@
+interface UserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface UserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: UserAddress;
+  company: UserCompany;
+}
+
+export default User;
