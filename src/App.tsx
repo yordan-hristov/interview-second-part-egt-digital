18e0681bcdb8a5b29820aba17a08c2dbf76dcf3c
Yordan Hristov <y.hristov11@gmail.com>
2023-04-14 18:14:17 +0300

    feat: home page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./components/home-page";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
