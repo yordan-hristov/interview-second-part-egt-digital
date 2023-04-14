82c1729fe0f796ddc271dd4e4b82c30a83b32891
Yordan Hristov <y.hristov11@gmail.com>
2023-04-14 19:04:53 +0300

    feat: fetch and display users

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { Collapse } from "antd";
+
+import axios from "utils/axios";
+
+import type User from "types/user";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const { data } = await axios.get<User[]>("users");
+
+      setUsers(data);
+    };
+
+    getUsers();
+  }, []);
+
+  return (
+    <Collapse>
+      {users.map(({ id, name, email }) => (
+        <Collapse.Panel key={id} header={`${name} (${email})`}>
+          Lorem ipsum dolor sit amet.
+        </Collapse.Panel>
+      ))}
+    </Collapse>
+  );
 }
 
 export default HomePage;
