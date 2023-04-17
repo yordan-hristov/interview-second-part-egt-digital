e6e61a5fa9fc928827472e9fb25f2645616aaa35
Yordan Hristov <y.hristov11@gmail.com>
2023-04-17 17:33:30 +0300

    feat: wrap App with redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 
+import store from "store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -13,7 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
