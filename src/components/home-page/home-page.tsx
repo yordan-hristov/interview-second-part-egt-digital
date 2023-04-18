f0da78565b2c26983f393f76f95f194b9ba8117b
Yordan Hristov <y.hristov11@gmail.com>
2023-04-18 14:02:07 +0300

    refactor: home page

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { Collapse, Spin, Typography } from "antd";
+import { Button, Collapse, Divider, Spin, Typography } from "antd";
 
 import { usersApi } from "store/users";
 import { useDispatch, useSelector } from "hooks/use-redux";
 import EditUser from "components/edit-user";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   const { users, isLoading, error } = useSelector((state) => state.users);
@@ -13,31 +14,50 @@ function HomePage() {
     dispatch(usersApi.getUsers());
   }, [dispatch]);
 
-  if (isLoading) {
-    return (
-      <Spin
-        style={{ position: "absolute", top: "50%", right: "50%" }}
-        size="large"
-      />
-    );
-  }
-
-  if (error) {
-    return (
-      <Typography.Text strong type="danger">
-        {error}
-      </Typography.Text>
-    );
-  }
-
   return (
-    <Collapse>
-      {users.map((user) => (
-        <Collapse.Panel key={user.id} header={`${user.name} (${user.email})`}>
-          <EditUser user={user} />
-        </Collapse.Panel>
-      ))}
-    </Collapse>
+    <div>
+      <Typography.Title level={1}>Users:</Typography.Title>
+
+      {isLoading && (
+        <Spin
+          style={{ position: "absolute", top: "50%", right: "50%" }}
+          size="large"
+        />
+      )}
+
+      {error && (
+        <Typography.Text strong type="danger">
+          {error}
+        </Typography.Text>
+      )}
+
+      {!isLoading && users && users.length <= 0 && (
+        <Typography.Text strong type="danger">
+          There are currently no users
+        </Typography.Text>
+      )}
+
+      {users && users.length > 0 && (
+        <Collapse>
+          {users.map((user) => (
+            <Collapse.Panel
+              key={user.id}
+              header={`${user.name} (${user.email})`}
+            >
+              <EditUser uid={user.id} />
+
+              <Divider />
+
+              <Link to={`posts?uid=${user.id}`}>
+                <Button size="large" type="default">
+                  See Posts
+                </Button>
+              </Link>
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      )}
+    </div>
   );
 }
 
