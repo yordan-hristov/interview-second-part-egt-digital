f786a3f3dfb475871526e5b04c64ed0c0ee42d61
Yordan Hristov <y.hristov11@gmail.com>
2023-04-18 19:11:25 +0300

    chore: delete obsolete label component

--- src/components/label/label.props.ts
+++
@@ -1,3 +0,0 @@
-export default interface LabelProps {
-  text: string;
-}
--- src/components/label/label.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "antd";
-
-import type LabelProps from "./label.props";
-
-function Label({ text }: LabelProps) {
-  return <Typography.Text strong>{text}</Typography.Text>;
-}
-
-export default Label;
