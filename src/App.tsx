e81cecbeb8e595cee800087583d476586136e756
Yordan Hristov <y.hristov11@gmail.com>
2023-04-19 11:35:23 +0300

    feat: router for user's posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 
 import HomePage from "components/home-page";
+import PostsPage from "components/posts-page";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/users/:uid/posts" element={<PostsPage />} />
     </Routes>
   );
 }
