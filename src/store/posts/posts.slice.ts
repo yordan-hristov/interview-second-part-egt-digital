a9be62ec17fc355551043d88a3fce28cc26cb988
Yordan Hristov <y.hristov11@gmail.com>
2023-04-19 20:05:54 +0300

    refactor: posts slice

--- src/store/posts/posts.slice.ts
+++ src/store/posts/posts.slice.ts
@@ -1,17 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, isPending } from "@reduxjs/toolkit";
 
 import { deletePost, getPosts, updatePost } from "./posts.api";
 
 import type Post from "types/post";
 
-const initialState: {
+interface PostsState {
   posts: Post[];
   isLoading: boolean;
   error: null | string;
-} = {
+  notification: {
+    type: "error" | "success";
+    value: string;
+  } | null;
+}
+
+const initialState: PostsState = {
   posts: [],
   isLoading: true,
   error: null,
+  notification: null,
 };
 
 const postsSlice = createSlice({
@@ -21,54 +28,56 @@ const postsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       // getPosts
-      .addCase(getPosts.pending, (state) => ({
-        ...state,
-        isLoading: true,
-      }))
-      .addCase(getPosts.fulfilled, (state, action) => ({
-        ...state,
-        posts: action.payload,
-        isLoading: false,
-      }))
-      .addCase(getPosts.rejected, (state, action) => ({
-        ...state,
-        isLoading: false,
-        error:
+      .addCase(getPosts.fulfilled, (state, action) => {
+        state.posts = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(getPosts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error =
           action.error.message ||
-          "Error occurred while trying to fetch posts for current users",
-      }))
+          "Error occurred while trying to fetch posts for current users";
+      })
       // updatePost
-      .addCase(updatePost.pending, (state) => ({
-        ...state,
-        isLoading: true,
-      }))
-      .addCase(updatePost.fulfilled, (state, action) => ({
-        ...state,
-        isLoading: false,
-      }))
-      .addCase(updatePost.rejected, (state, action) => ({
-        ...state,
-        isLoading: false,
-        error:
+      .addCase(updatePost.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.notification = {
+          type: "success",
+          value: "Post updated successfully",
+        };
+      })
+      .addCase(updatePost.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error =
           action.error.message ||
-          "Error occurred while trying to update the post",
-      }))
+          "Error occurred while trying to update the post";
+        state.notification = {
+          type: "error",
+          value: "Updating post failed",
+        };
+      })
       // deletePost
-      .addCase(deletePost.pending, (state, action) => ({
-        ...state,
-        isLoading: true,
-      }))
-      .addCase(deletePost.fulfilled, (state, action) => ({
-        ...state,
-        isLoading: false,
-      }))
-      .addCase(deletePost.rejected, (state, action) => ({
-        ...state,
-        isLoading: false,
-        error:
+      .addCase(deletePost.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.notification = {
+          type: "success",
+          value: "Post deleted successfully",
+        };
+      })
+      .addCase(deletePost.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error =
           action.error.message ||
-          "Error occurred while trying to delete the post",
-      }));
+          "Error occurred while trying to delete the post";
+        state.notification = {
+          type: "error",
+          value: "Deleting post failed",
+        };
+      })
+      // Pending matcher
+      .addMatcher(isPending, (state) => {
+        state.isLoading = true;
+      });
   },
 });
 
