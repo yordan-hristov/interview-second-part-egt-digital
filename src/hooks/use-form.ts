9c947f33002a2dc72648103cc5fffc5bc049a2dc
Yordan Hristov <y.hristov11@gmail.com>
2023-04-19 14:47:36 +0300

    refactor: extract repeating login in custom hook

---
+++ src/hooks/use-form.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Form } from "antd";
+
+function useForm(initialFormValues: Record<string, string>) {
+  const [dirtyFields, setDirtyFields] = useState<string[]>([]);
+  const [form] = Form.useForm();
+
+  const handleValuesChange = (changed: Partial<typeof initialFormValues>) => {
+    const [key] = Object.keys(changed);
+    const castedKey = key as keyof typeof changed;
+
+    const currentValue = changed[castedKey];
+
+    if (currentValue?.trim() !== initialFormValues[castedKey].trim()) {
+      setDirtyFields((prev) => [...prev, castedKey]);
+    } else {
+      setDirtyFields((prev) => prev.filter((k) => k !== castedKey));
+    }
+  };
+
+  const handleResetFields = () => {
+    form.resetFields();
+    setDirtyFields([]);
+  };
+
+  const isDirty = dirtyFields.length > 0;
+
+  return { form, isDirty, handleValuesChange, handleResetFields };
+}
+
+export default useForm;
