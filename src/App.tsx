0b7946b6485b29e11758797279a67001217b5cf1
Yordan Hristov <y.hristov11@gmail.com>
2023-04-19 19:09:45 +0300

    feat: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Routes, Route } from "react-router";
+import { Layout } from "antd";
 
 import HomePage from "components/home-page";
 import PostsPage from "components/posts-page";
+import LayoutMenu from "components/layout-menu";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/users/:uid/posts" element={<PostsPage />} />
-    </Routes>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Layout.Sider>
+        <LayoutMenu />
+      </Layout.Sider>
+      <Layout.Content style={{ padding: "0 16px" }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/users/:uid/posts" element={<PostsPage />} />
+        </Routes>
+      </Layout.Content>
+    </Layout>
   );
 }
 
