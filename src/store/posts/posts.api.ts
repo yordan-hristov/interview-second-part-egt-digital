115026680488da0e88bace7e6bff723aec8d3db5
Yordan Hristov <y.hristov11@gmail.com>
2023-04-19 11:45:37 +0300

    feat: getPosts async thunk

---
+++ src/store/posts/posts.api.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "utils/axios";
+
+import type Post from "types/post";
+
+export const getPosts = createAsyncThunk(
+  "posts/getPosts",
+  async (uid: number) => {
+    const { data } = await axios.get<Post[]>(`users/${uid}/posts`);
+
+    return data;
+  }
+);
