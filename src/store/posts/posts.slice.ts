35f20239eb47aceaaa14715d231cc905ec90a463
Yordan Hristov <y.hristov11@gmail.com>
2023-04-19 11:46:05 +0300

    feat: getPosts reducers

--- src/store/posts/posts.slice.ts
+++ src/store/posts/posts.slice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { getPosts } from "./posts.api";
+
 import type Post from "types/post";
 
 const initialState: {
@@ -16,6 +18,25 @@ const postsSlice = createSlice({
   initialState,
   name: "posts",
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPosts.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(getPosts.fulfilled, (state, action) => ({
+        ...state,
+        posts: action.payload,
+        isLoading: false,
+      }))
+      .addCase(getPosts.rejected, (state, action) => ({
+        ...state,
+        isLoading: false,
+        error:
+          action.error.message ||
+          "Error occurred while trying to fetch posts for current users",
+      }));
+  },
 });
 
 export default postsSlice;
